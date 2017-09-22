ce9e499e6a9057d322e84045707b563b1ff13f2e
Aiza <aiza.anwar@gmail.com>
2017-09-22 11:55:14 +0500

    aiza change

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -17,7 +17,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hello Aiza");
+        System.out.println("Hello Aiza Anwar");
         System.out.println("Hello Ammad");
 
     }
